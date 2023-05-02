f385da48c18cf22eb2bba00d4b07b9abb975a6da
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-05-02 21:30:58 +0400

    create Record_static_extension folder

---
+++ Record_Static_Extension/Program.cs
@@ -0,0 +1,11 @@
+﻿using Record_Static_Extension.Models;
+using Record_Static_Extension.Utilies;
+
+//Student student1 = new Student();
+//Student student2 = new Student();
+//Student student3 = new Student();
+//Console.WriteLine(student1.Id);
+//Console.WriteLine(student2.Id);
+//Console.WriteLine(student3.Id);
+string test = "Salam nicat necesen nicat ay nicat";
+test.MatchCount("nicat");
