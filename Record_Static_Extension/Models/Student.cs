977afcb6c3b71eb027155e9e33f85f120064c8b5
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-05-02 20:27:09 +0400

    create Student file

---
+++ Record_Static_Extension/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace Record_Static_Extension.Models;
+
+internal class Student
+{
+    public int Id { get; }
+    private static int Count { get; set; }
+
+    public Student()
+    {
+        Count++;
+        Id = Count;
+    }
+
+
+}
