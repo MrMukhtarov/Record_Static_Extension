f26ad3b4b24f84bbf9582103e8a69be341bdc032
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-05-02 21:30:18 +0400

    create Extension file

---
+++ Record_Static_Extension/Utilies/Extension.cs
@@ -0,0 +1,26 @@
+﻿namespace Record_Static_Extension.Utilies;
+
+public static class Extension
+{
+    public static void MatchCount(this string str, string findStr)
+    {
+        int count = 0;
+        for (int i = 0; i <= str.Length - findStr.Length; i++)
+        {
+            bool match = true;
+            for (int j = 0; j < findStr.Length; j++)
+            {
+                if (str[i + j] != findStr[j])
+                {
+                    match = false;
+                    break;
+                }
+            }
+            if (match)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
